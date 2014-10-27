3142d190cada69f3774dcc7647000773b2c8f96b
Juan Negretti <wang@Wang-PC.ciens.ucv.ve>
2014-10-27 10:47:03 -0430

    URLoader now count words and other stuff

--- lib/DataMining/URLoader.pm
+++ lib/DataMining/URLoader.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Carp;
 require LWP::UserAgent;
-use moose;
+use Moose;
 use List::Util qw/reduce/;
 use namespace::autoclean;
 
@@ -25,7 +25,7 @@ has 'content' => (
 has 'status' => (
     is          =>  'ro',
     isa         =>  'Int',
-    writer      =>  '_status'
+    writer      =>  '_status',
     init_arg    =>  undef,
 );
 
@@ -52,6 +52,35 @@ has 'count' => (
     isa         =>  'HashRef',
     writer      =>  '_count',
     init_arg    =>  undef,
+    default     => sub { return {}; },
+);
+
+has 'polynomials' => (
+    is          =>  'rw',
+    isa         =>  'HashRef',
+    default     =>  sub {
+        return {
+            business => { 
+                constant    =>  -0.91,
+                local       =>  -0.21,
+                coach       =>  0.59,
+                manag       =>  0.39,
+                startup     =>  1.49,
+                real        =>  -0.43,
+                magazin     =>  -1.38,
+                compani     =>  0.35,
+                citi        =>  0.1,
+                design      =>  0.13,
+                equal       =>  -1.02,
+                market      =>  0.62
+            },
+            entertainment => {
+                constant    =>  -0.67,
+                singer      =>  1.8,
+                manag       =>  -0.57,
+            }
+        };
+    };
 );
 
 sub BUILD {
@@ -61,6 +90,7 @@ sub BUILD {
     $ua->agent("DataMining/0.1");
 
     $self->_client($ua);
+    $self->load_words;
 }
 
 sub load_words {
@@ -70,20 +100,20 @@ sub load_words {
     croak 'Words must be set' if !$self->words;
 
     # SOME HACK!!!!
-    $self->words->[0] = '(' . $self->words->[0] . ')';
-    my $regex = reduce { $a . '|(' . $b . ')'; } @{$self->words};
+    $self->words->[0] = '(?:' . $self->words->[0] . ')';
+    my $regex = reduce { $a . '|(?:' . $b . ')'; } @{$self->words};
 
-    $self->regex(qr/$regex/);
+    $self->_regex(qr/($regex)/);
 }
 
-sub load {
+sub do_request {
     my ($self, $url);
 
     $self->url($url) if $url;
     croak 'URL must be set' if !$self->url;
     my $request = HTTP::Request->new(GET => $self->url);
 
-    my $res = $self->_client->request($req);
+    my $res = $self->_client->request($request);
 
     $self->_status($res->code);
     $self->_content($res->content);
@@ -91,8 +121,21 @@ sub load {
     return $self;
 }
 
-sub count {
+sub count_words {
     my ($self) = @_;
+
+    $self->do_request if !$self->content;
+    croak 'The status must be ok' if $self->status != 200;
+
+    my ($regex, $content) = ($self->regex, $self->content);
+
+    while ( $content =~ m/$regex/g ) {
+        $self->count->{$1}++;
+    }
 
 
 }
+
+__PACKAGE__->meta->make_immutable;
+
+1;
