5d19666c3647aafa137c7d76af98706169b7bce0
Juan Negretti <wang@Wang-PC.ciens.ucv.ve>
2014-10-30 11:57:54 -0430

    finished URLoader

--- lib/DataMining/URLoader.pm
+++ lib/DataMining/URLoader.pm
@@ -5,7 +5,7 @@ use warnings;
 use Carp;
 require LWP::UserAgent;
 use Moose;
-use List::Util qw/reduce/;
+use List::Util qw/reduce sum/;
 use namespace::autoclean;
 
 has 'client' => (
@@ -44,7 +44,47 @@ has 'regex' => (
 has 'words' => (
     is          =>  'rw',
     isa         =>  'ArrayRef',
-    required    =>  1,
+    init_arg    =>  undef,
+    default     =>  sub {
+        return [
+            'local',
+            'coach',
+            'manag',
+            'startup',
+            'real',
+            'magazin',
+            'compani',
+            'citi',
+            'design',
+            'equal',
+            'market',
+            'singer',
+            'tv',
+            'locat',
+            'februari',
+            'episod',
+            'leagu',
+            'player',
+            'club',
+            'defend',
+            'stadium',
+            'bosniawhi',
+            'nasa',
+            'star',
+            'travel',
+            'play',
+            'hide',
+            'stuff',
+            'trial',
+            'digit',
+            'tool',
+            'english',
+            'told',
+            'won',
+            'equal',
+            'tech',
+        ];
+    },
 );
 
 has 'count' => (
@@ -78,9 +118,57 @@ has 'polynomials' => (
                 constant    =>  -0.67,
                 singer      =>  1.8,
                 manag       =>  -0.57,
+                tv          =>  0.39,
+                locat       =>  -0.58,
+                februari    =>  1.53,
+                design      =>  -0.15,
+                market      =>  -0.4,
+                episod      =>  1.52,
+            },
+            football => {
+                constant    =>  -6.05,
+                leagu       =>  2.37,
+                player      =>  0.78,
+                club        =>  0.11,
+                defend      =>  0.91,
+                manag       =>  0.46,
+                stadium     =>  0.91,
+            },
+            science => {
+                constant    =>  -11.84,
+                bosniawhi   =>  24.41,
+                nasa        =>  0.18,
+            },
+            technology => {
+                constant    =>  -1.43,
+                club        =>  0.44,
+                star        =>  0.07,
+                coach       =>  -0.25,
+                travel      =>  -1.49,
+                play        =>  0.35,
+                manag       =>  -0.74,
+                hide        =>  -1,
+                nasa        =>  0.76,
+                stuff       =>  1.06,
+                trial       =>  0.46,
+                digit       =>  0.62,
+                tool        =>  0.54,
+            },
+            travel => {
+                constant    =>  -1.57,
+                english     =>  0.4,
+                local       =>  0.43,
+                told        =>  -0.27,
+                won         =>  0.05,
+                travel      =>  0.91,
+                hide        =>  2,
+                tv          =>  -0.17,
+                equal       =>  1.55,
+                tech        =>  -0.39,
+                tool        =>  -0.22,
             }
         };
-    };
+    },
 );
 
 sub BUILD {
@@ -107,13 +195,13 @@ sub load_words {
 }
 
 sub do_request {
-    my ($self, $url);
+    my ($self, $url) = @_;
 
     $self->url($url) if $url;
     croak 'URL must be set' if !$self->url;
     my $request = HTTP::Request->new(GET => $self->url);
 
-    my $res = $self->_client->request($request);
+    my $res = $self->client->request($request);
 
     $self->_status($res->code);
     $self->_content($res->content);
@@ -134,6 +222,29 @@ sub count_words {
     }
 
 
+}
+
+sub calculate_probabilities {
+    my ($self) = @_;
+
+    croak 'words has not been counted' if !scalar(keys %{$self->count});
+
+    my ($sum,$var,%calculated_poly);
+
+    for my $category ( keys %{ $self->polynomials } ) {
+        $calculated_poly{$category} = $self->polynomials->{constant};
+
+        for ( keys %{ $self->polynomials->{$category} } ) {
+            $var = ($self->count->{$_})? $self->count->{$_} : 0;
+            $calculated_poly{$category} += $self->polynomials->{$category}->{$_} * $var;
+        }
+
+        $sum += exp( $calculated_poly{$category} );
+    }
+
+    my %probs = map { ( $_ => exp( $calculated_poly{$_} )/$sum ) } keys %calculated_poly;
+
+    return \%probs;
 }
 
 __PACKAGE__->meta->make_immutable;
